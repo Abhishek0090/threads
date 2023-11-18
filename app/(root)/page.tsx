37b897b6c68bbd8f360b40be240a314b3ca927d4
Abhishek0090 <palabhishek411@gmail.com>
2023-11-19 00:19:24 +0530

    added components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+    </div>
+  );
+}
